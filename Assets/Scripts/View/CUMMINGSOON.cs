c4ac88614d6181905478849f7a9334adc1faed8b
Anton Vorobev <anonspirov@yandex.ru>
2021-12-18 22:09:17 +0400

    wip

---
+++ Assets/Scripts/View/CUMMINGSOON.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.View
+{
+    internal class CUMMINGSOON : MonoBehaviour
+    {
+        public Button Button;
+
+        private void Awake()
+        {
+            Button.onClick.AddListener(() => Fader.instance.ShowMessage("CUMING SOON", null));
+        }
+    }
+}
