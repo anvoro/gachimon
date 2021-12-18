637a93b20e6c88c10c0a9e343e902d9e49552095
Anton Vorobev <anonspirov@yandex.ru>
2021-12-18 21:21:55 +0400

    wip

---
+++ Assets/Scripts/View/TOBattle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.View
+{
+    internal class TOBattle : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _button;
+
+        private void Awake()
+        {
+            SceneManager.LoadScene(1, LoadSceneMode.Additive);
+
+            this._button.onClick.AddListener(() => this.GoToBattle());
+        }
+
+        private void GoToBattle()
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
